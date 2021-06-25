5b6bffa46a096c984e1ed6e348f289fa96b3e565
Igor Gonçalves <igordossantos@gmail.com>
2021-06-25 17:19:31 -0300

    Alterações

--- script.js
+++ script.js
@@ -1,24 +1,17 @@
 function start() {
     let zod = document.querySelector('p.zod');
     let dia = document.querySelector('input.txtd');
-
-    let mes = document.querySelector('select.txtm'); //Captura nao esta acontecendo
+    let mes = document.querySelector('select.txtm');
     
      if (dia.value.length == 0 || dia.value > 31 || dia.value == 0)  { //Alertas bem sucedidos
          window.alert('[ERRO] Digite o dia nascimento.');
          zod.innerHTML = 'testando conexão'; /*Captura bem sucessida!*/
-
-        } else if (mes.value == "0") { //Captura bem sucesssida
-          window.alert('deu certo')
+         
+        } else if (Number(dia.value) >= 21 && mes.value == "0" ) { 
+          window.alert('deu certo') /*Captura bem sucessida!*/
+          zod.innerHTML = 'Áries';
         }
 }
-
-else if (Number(dia.value) >= 21) { //Captura e conversao ok funcionando
-  zod.innerHTML = 'Áries';
-
-
-
-
 
 /* Use o switch case ........
 Áries: de 21 março a 20 abril
@@ -32,4 +25,7 @@ Escorpião: de 23 outubro a 21 novembro
 Sagitário: de 22 novembro a 21 dezembro
 Capricórnio: de 22 dezembro a 20 janeiro
 Aquário: de 21 janeiro a 18 fevereiro
-Peixes: de 19 fevereiro a 20 março. */
+Peixes: de 19 fevereiro a 20 março. 
+
+jan "0" fev "1" mar "2" abr "3" mai "4" jun "5"
+jul "6" ago "7" set "8" out "9" nov "10" dez "11" */
